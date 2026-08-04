b0377f2d4f8c672c7184207aeee1fed12e47654e
pnu <pnu2410@gmail.com>
2026-08-04 19:44:19 +0700

    Update player movement, add collision and obstacle

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -3,19 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 moveVector;
 
-    [SerializeField] float moveSpeed = 8f;
+    private Vector2 moveVector;
+
+    [SerializeField] private float moveSpeed = 10f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void InputPlayer(InputAction.CallbackContext context)
     {
         moveVector = context.ReadValue<Vector2>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveVector.x, 0f, moveVector.y);
-        movement.Normalize();
-        transform.Translate(moveSpeed * movement * Time.deltaTime);
+
+        if (movement.sqrMagnitude > 1f)
+            movement.Normalize();
+
+        rb.linearVelocity = new Vector3(
+            movement.x * moveSpeed,
+            rb.linearVelocity.y,
+            movement.z * moveSpeed
+        );
     }
 }
